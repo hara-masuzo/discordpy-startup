ec744d1b876c07261d6ddf7e21c278ace9320f0c
hara-masuzo <51125120+hara-masuzo@users.noreply.github.com>
2021-07-13 02:23:41 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -129,16 +129,4 @@ while True:
     time.sleep(60)
 
 
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
-    
-    
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
-
-
 bot.run(token)
