be698f2f7cf511487ecbb709094abd5826cd73d6
hara-masuzo <51125120+hara-masuzo@users.noreply.github.com>
2021-07-13 02:30:52 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -33,7 +33,7 @@ webhook_url_Hololive = '配信開始チャンネル用のwebhookリンク' #ホ�
 webhook_url_Hololive_yotei = '配信開始予定用のwebhookリンク' #ホロライブ配信予定
 broadcast_data = {} #配信予定のデータを格納
 
-YOUTUBE_API_KEY = []
+YOUTUBE_API_KEY = [複数のAPI(str型)をリストで管理]
 
 def dataformat_for_python(at_time): #datetime型への変換
     at_year = int(at_time[0:4])
